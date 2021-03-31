8819503595e8134dcfdc8cbf25cb128b4bf3dcd5
AXT-web <m18775921835@163.com>
2021-03-31 16:43:34 +0800

    完成了搜素功能

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		
+		Goods_list
 	</view>
 </template>
 
@@ -12,22 +12,6 @@
 			};
 		}
 	}
-  methods : {
-    // 获取楼层列表数据
-    async getFloorList() {
-      const { data: res } = await uni.$http.get('/api/public/v1/home/floordata')
-      if (res.meta.status !== 200) return uni.$showMsg()
-    
-      // 通过双层 forEach 循环，处理 URL 地址
-      res.message.forEach(floor => {
-        floor.product_list.forEach(prod => {
-          prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
-        })
-      })
-    
-      this.floorList = res.message
-    }
-  }
 </script>
 
 <style lang="scss">
